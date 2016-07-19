09dd1dc8e2119bb5a678b3dec29aab9515136f12
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-07-19 16:22:43 -0400

    updating JQ

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,36 @@
 
 $(document).ready(function(){
-		// random clicked
-    $(".randomNum").on("click", function(){
-        var random = Math.floor(Math.random() * 120) + 19;  
-        $('#randomNum').html(random);
+		// random number at clicked
+	function randomNum() {
+         var theNumber = Math.floor(Math.random() * 120) + 19;
+         $(".randomNum").html(theNumber);
+		console.log(theNumber);
+};
+randomNum();
+    //*Adding a function for each gem so they can generate a random number from 1 to 12.
+    //*redgem
+    $(".redgem").on("click", function(){
+        var random = Math.floor(Math.random() * 12) + 1 ;  
+        $(".totalNum").html(random);
          console.log(random);
     })
+    //*bluegem
+    $(".bluegem").on("click", function() {
+    	var random = Math.floor(Math.random() * 12) + 1 ;
+    	$(".totalNum").html(random);
+    	console.log(random);
+    })
+    //*yellowgem
+   $(".yellowgem").on("click", function() {
+   		var random = Math.floor(Math.random () * 12) + 1 ;
+   		$(".totalNum").html(random);
+   		console.log(random);
+   })
+   //*greengem
+   $(".greengem").on("click", function() {
+   		var random =Math.floor(Math.random () *12) + 1;
+   		$(".totalNum").html(random);
+   		console.log(random);
+   	})
    
 })

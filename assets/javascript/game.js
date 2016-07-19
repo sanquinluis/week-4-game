04df620bef3c8d8f0bb277941233306256c10a59
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-07-19 18:21:07 -0400

    JQ

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,27 @@
 
 $(document).ready(function(){
-		// random number at clicked
+	 var totalNum = 0;
+	 var redgem = 0;
+	 var bluegem = 0;
+	 var yellowgem = 0;
+	 var greengem = 0;
+		// random number wen clicked
 	function randomNum() {
          var theNumber = Math.floor(Math.random() * 120) + 19;
          $(".randomNum").html(theNumber);
 		console.log(theNumber);
 };
-randomNum();
+		randomNum();
     //*Adding a function for each gem so they can generate a random number from 1 to 12.
-    //*redgem
-    $(".redgem").on("click", function(){
+    $(".redgem, .bluegem, .yellowgem, .greengem").on("click", function(){
         var random = Math.floor(Math.random() * 12) + 1 ;  
         $(".totalNum").html(random);
          console.log(random);
-    })
-    //*bluegem
-    $(".bluegem").on("click", function() {
-    	var random = Math.floor(Math.random() * 12) + 1 ;
-    	$(".totalNum").html(random);
-    	console.log(random);
-    })
-    //*yellowgem
-   $(".yellowgem").on("click", function() {
-   		var random = Math.floor(Math.random () * 12) + 1 ;
-   		$(".totalNum").html(random);
-   		console.log(random);
+  
+  	$(".totalNum").html (function (){
+   		totalNum = random + redgem + bluegem + yellowgem + greengem;
+   		console.log(totalNum);
+   		})
    })
-   //*greengem
-   $(".greengem").on("click", function() {
-   		var random =Math.floor(Math.random () *12) + 1;
-   		$(".totalNum").html(random);
-   		console.log(random);
-   	})
    
 })

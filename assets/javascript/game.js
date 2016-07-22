6eb0ff7c576718413439cb83bfa27dc0bbf2ed4f
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-07-22 02:19:35 -0400

    fixing a bug

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,20 +5,9 @@ $(document).ready(function(){
 	var yourTotal = 0;
 	var crystals = {redgem:{name:"red",value: 0}, bluegem:{name:"blue", value: 0},yellowgem:{name:"yellow",value: 0}, greengem:{name:"green",value:0}
 	}
-	var random = function(min,max){
-	 return Math.floor(Math.random()* (max-min+1)) +min;
-	}
-		//restart and reset "the game"
-	 var restart = function (){
-		var wins = 0; var losses = 0; yourTotal = 0;
-		$(".totalScore").html(yourTotal);
-		$(".wins").html(wins);
-		$(".losses").html(losses);
-		var winningNum = Math.floor(Math.random() * (120 - 19 +1)) + 19;
-		$(".randomNum").on ("click", winningNum);
-		$(".randomNum").css("fontSize", "15px");
-		$(".winningNum").html(totalNum);
-	}
+	// var random = function(min,max){
+	//  return Math.floor(Math.random()* (max-min+1)) +min;
+	// }
 	    //generates a randomnum from 120 to 19
 	var winningNum = Math.floor(Math.random() * (120 - 19 +1)) + 19;
 		$(".randomNum").html (winningNum);
@@ -55,6 +44,17 @@ $(document).ready(function(){
 			console.log("greengem value = " + crystals.greengem.value);
 			totalNum(crystals.greengem.value);
 	 	});
+	 			// Reset the game... 	
+			function clear() {
+        		$( ".randomNum").empty();
+        		$( ".redgem bluegem yellowgem greengem" ).empty();
+        		randomNum= [];
+      }
+      		function reset() {
+        		totalNum();
+        		winningNum();
+        		yourTotal();
+      }
 	 	function totalNum (valor) {
 	 		yourTotal += valor;
 	 		console.log("Your total = " + yourTotal);
@@ -62,19 +62,25 @@ $(document).ready(function(){
 	 		$(".totalNum").html(yourTotal);
 	 		if (yourTotal == winningNum){
 	 			wins ++;
+	 			reset();
 	 			console.log("wins" + wins);
 	 			console.log("You Win!!");
-	 			$(".winslosses").html("Wins " + wins);	
-	 			//$(".winslosses").html("You Win!")
+	 			$(".wins").html("Wins " + wins);	
+	 			$(".yousomething").html("You Win!")
 	 		}
 	 		else if (yourTotal > winningNum){
 	 			losses ++;
+	 			reset;
 	 			console.log("looses" + losses);
 	 			console.log("You Looses!!");
-	 			$(".winslosses").html("Losses " + losses);
-	 			$(".winslosses").html("You lose!!");
+	 			$(".losses").html("Losses " + losses);
+	 			$(".yousomething").html("You lose!!");
 	 			$(".winningNum").html(winningNum);
-	 		}
+	 			
+	}
+		  
+	 		
+	 		
 
 	 	}
 
